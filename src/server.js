f81b3d29a78f38d23064f0b4ba510d6eaa708915
Pedro Meira <petergamer8k@gmail.com>
2022-08-19 18:47:40 -0300

    Fixing an database connection error

--- src/server.js
+++ src/server.js
@@ -1,19 +1,16 @@
 import express from "express"
 
+import pkg from "pg"
+const { Pool } = pkg
+
 const app = express()
+
+app.use(express.json())
 
 app.get("/", async (req, res) => {
   //const response = await databaseQuery("select * from users")
 
-  res.send("Hello world!!!")
-})
-
-app.get("/user", (req, res) => {
-  return res.send("user's route")
-})
-
-app.get("/test", (req, res) => {
-  return res.send("user's route")
+  res.send("Welcome to my api")
 })
 
 const PORT = 3333
@@ -21,30 +18,21 @@ const PORT = 3333
 app.listen(PORT, () => {
   console.log(`Http server running at ${PORT}`)
 })
-/*
-const { Pool } = require("pg")
 
 // Conexão com a Base de Dados:
 
 const pool = new Pool({
   user: "postgres",
-  host: "localhost",
+  host: "host.docker.internal",
   database: "my_database",
   password: "password",
   port: "3001",
 })
 
-pool.on("connect", () => {
-  console.log("Database successfully connected!")
-})
-
 pool.on("error", (error) => {
-  console.log(error)
-
-  throw new Error("Internal server error")
+  //console.log(error)
 })
 
 function databaseQuery(text, params) {
   return pool.query(text, params)
 }
-*/
